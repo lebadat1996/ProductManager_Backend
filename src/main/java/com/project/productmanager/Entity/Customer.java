e3a9f4e97d3a63fb6da1a24acb721ca1878f4ba3
lebadat1996ptit@gmail.com <lebadat1996ptit@gmail.com>
2021-05-13 21:49:57 +0700

    create infomation  Customer

--- src/main/java/com/project/productmanager/Entity/Customer.java
+++ src/main/java/com/project/productmanager/Entity/Customer.java
@@ -1,4 +1,21 @@
 package com.project.productmanager.Entity;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "CUSTOMER")
+@Data
 public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(columnDefinition = "CUSTOMER_ID")
+    private Integer customerID;
+    @Column(columnDefinition = "FULL_NAME")
+    private String fullName;
+    @Column(columnDefinition = "PHONE")
+    private String phone;
+    @Column(columnDefinition = "ADDRESS")
+    private String address;
 }
